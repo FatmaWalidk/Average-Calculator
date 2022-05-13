b6111dcc6383fa72ec23f9c71dfdc0cef42f3ed5
Sama <s.m.alfadhli@gmail.com>
2022-05-13 14:14:03 +0300

    added stdno... Added total class average

--- src/components/CalculateAverage.js
+++ src/components/CalculateAverage.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React from "react";
 
-export const CalculateAverage = ({FN, LN, A1, A2, A3}) => {
-    return (
+export const CalculateAverage = ({ FN, LN, A1, A2, A3, stdNo }) => {
+  return (
     <>
-        <td>{FN}</td>
-        <td>{LN}</td>
-        <td>{A1}</td>
-        <td>{A2}</td>
-        <td>{A3}</td>
-        <td data-testid="Class Average">{((A1*0.3)+(A2*0.3)+(A3*0.4)).toLocaleString(undefined, {maximumFractionDigits:2}) }</td>
+      <td>{stdNo}</td>
+      <td>{FN}</td>
+      <td>{LN}</td>
+      <td>{A1}</td>
+      <td>{A2}</td>
+      <td>{A3}</td>
+      <td data-testid="Class Average">
+        {(A1 * 0.3 + A2 * 0.3 + A3 * 0.4).toLocaleString(undefined, {
+          maximumFractionDigits: 2,
+        })}
+      </td>
     </>
-    );
-}
- 
+  );
+};
+
 export default CalculateAverage;
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,29 +1,50 @@
-import React from 'react';
-import {CalculateAverage} from './CalculateAverage';
-import './Data.css'
-import './GetExcel.css';
+import React, { useEffect, useState } from "react";
+import { CalculateAverage } from "./CalculateAverage";
+import "./Data.css";
+import "./GetExcel.css";
 
-export const Data = ({excelData}) => {
-    return(   
-        <div> 
-            <br/> <br/>
-            
-            <table>
-            <thead> 
-                    <tr>
-                        <th> First Name </th>
-                        <th> Last Name </th> 
-                        <th> Assessment 1</th> 
-                        <th> Assessments 2</th>
-                        <th> Assessments 3</th>
-                        <th> Class Average </th> 
-                    </tr>
-                </thead>
-                <tbody>
-                    {excelData.map((data)=>(<tr key={data.ID}><CalculateAverage FN={data.First_Name} LN={data.Last_Name} A1={data.A1} A2={data.A2} A3={data.A3}/></tr>))}
-                </tbody>
-            </table>
-        </div>
-    )
-}
- 
+export const Data = ({ excelData }) => {
+  const [classAverage, setclassAverage] = useState("0");
+  useEffect(() => {
+    var total = 0;
+    excelData.forEach(
+      (eachData) =>
+        (total += eachData.A1 * 0.3 + eachData.A2 * 0.3 + eachData.A3 * 0.4)
+    );
+    setclassAverage((total / excelData.length).toFixed(2));
+  }, [excelData]);
+
+  return (
+    <div>
+      <br /> <br />
+      <table>
+        <caption>Class average: {classAverage}</caption>
+        <thead>
+          <tr>
+            <th> Student Number </th>
+            <th> First Name </th>
+            <th> Last Name </th>
+            <th> Assessment 1</th>
+            <th> Assessments 2</th>
+            <th> Assessments 3</th>
+            <th> Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {excelData.map((data) => (
+            <tr key={data.ID}>
+              <CalculateAverage
+                FN={data.First_Name}
+                LN={data.Last_Name}
+                A1={data.A1}
+                A2={data.A2}
+                A3={data.A3}
+                stdNo={data.stdNo}
+              />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
