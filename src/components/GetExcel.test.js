7cb959218803e1dc0c9478709a780da47d7d7da4
fatima <Fatimawalidk@gmail.com>
2022-05-15 19:14:47 +0300

    Refactoring 1

--- src/components/GetExcel.test.js
+++ src/components/GetExcel.test.js
@@ -2,13 +2,6 @@ import { render, screen } from '@testing-library/react';
 import GetExcel from './GetExcel';
 
 test('It should have a file input' , () =>  { 
-
-});
-
-test('It should have a submit button' , () =>  {
-});
-/*
-test('It should have a file input' , () =>  { 
   render(<GetExcel />);
   expect(screen.getByLabelText(/Your Excel file:/)).toBeInTheDocument();
 });
@@ -18,9 +11,12 @@ test('It should have a submit button' , () =>  {
   expect(screen.getByText('Submit')).toBeInTheDocument();
 });
 
+// ********* Second sprint test cases *********
+test('t should go to the home page when clicking the resrt button' , () =>  {
+  render(<GetExcel />);
+  expect(screen.getByText('Reset')).toBeInTheDocument();
+});
 
-
-*/
 
 
 
